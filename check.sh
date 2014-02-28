a58b8017ee40ff22504db717845cb81bdb641b39
Pablo Torres <pablo.torres@mercadolibre.com>
2014-02-28 12:51:26 -0300

    check

--- check.sh
+++ check.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-SITE=$1
-ENV=$2
+GIT_STATUS=`git status --porcelain`
 
-FILE="seo_configuration_$SITE.json"
-
-./download.sh $SITE
-
-git add -A
-git diff --cached -- files/$FILE > temporal/diff 
-patch -R -o temporal/reverted files/$FILE temporal/diff
-
-DIFF=`diff temporal/reverted swift_files/$FILE`
-
-if [ -n "$DIFF" ] ; then
-	echo "You are not updated from swift, download swift file before modify it"
-	exit 1
+if [ "$GIT_STATUS" == "" ]; then
+    echo "nada modificado"
+else
+    echo "algo se modifico"
 fi
 
 exit 0
