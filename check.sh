0729515da5231450fa1e1cb1e75b4357e74f34b5
Pablo Torres <pablo.torres@mercadolibre.com>
2014-02-28 13:10:05 -0300

    a

--- check.sh
+++ check.sh
@@ -3,17 +3,19 @@
 GIT_STATUS=`git status --porcelain`
 
 if [ "$GIT_STATUS" == "" ]; then
-    echo "no hay nada para comitear"
+    echo "check git status : OK (nothing to commit)"
 else
-    echo "hay cosas modificadas localmente sin comitear , ejecuta un git status"
+    echo "check git status : FAIL (there are local changes not commited, execute 'git status')"
+    exit 1
 fi
 
 GIT_PUSH_STATUS=`git push --porcelain --dry-run origin master 2>&1 | grep "up to date"`
 
 if [ "$GIT_PUSH_STATUS" == "" ]; then
-    echo "either you're behind or ahead of origin master"
+    echo "check git push : FAIL (either you're behind or ahead of origin master, execute 'git push')"
+    exit 2
 else
-    echo "up to date"
+    echo "check git push : OK (you're up to date with gitHub repo)"
 fi
 
 exit 0
